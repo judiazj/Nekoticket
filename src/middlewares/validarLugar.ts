6ad80b4b8b30bd55d0102a5d13a27cd1086fbcb4
Judiazj <judiazj@unal.edu.co>
2024-08-28 19:14:22 -0500

    Middleware lugar arreglado

--- src/middlewares/validarLugar.ts
+++ src/middlewares/validarLugar.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import PlaceModel from '../models/place.js';
 
 export const existeLugarPorId = async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-    const lugar = await PlaceModel.findById(id);
+    const { id_lugar } = req.body;
+    const lugar = await PlaceModel.findById(id_lugar);
     if (!lugar) {
         return res.status(400).json({
-            msg: `El lugar con id ${id} no existe`
+            msg: `El lugar con id ${id_lugar} no existe`
         });
     }
     next();
