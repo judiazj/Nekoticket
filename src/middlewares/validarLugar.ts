eb43b773565b3c34041f5805d7b01a001ad2688c
Judiazj <judiazj@unal.edu.co>
2024-08-28 19:03:50 -0500

    validar lugar actualizado

--- src/middlewares/validarLugar.ts
+++ src/middlewares/validarLugar.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import PlaceModel from '../models/place.js';
 
 export const existeLugarPorId = async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.body;
+    const { id } = req.params;
     const lugar = await PlaceModel.findById(id);
     if (!lugar) {
         return res.status(400).json({
